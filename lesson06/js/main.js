a2ad1d2599a8e776b63eb0a210c7c20fbee022b2
huangran <huangran@pyyx.com>
2021-03-24 10:57:45 +0800

    简述：添加lesson6，7
    背景信息：
    
    内容描述：
    
    对其他模块的影响：
        [无]
    
    对测试组的影响：
        [无]
    
    后续遗留问题：
        [无]

--- lesson06/js/main.js
+++ lesson06/js/main.js
@@ -7,6 +7,7 @@ function randomMinMax(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+// 生成一个RGB随机颜色
 function randomColor() {
     return 'rgb(' +
         randomMinMax(0, 255) + ',' +
@@ -14,6 +15,7 @@ function randomColor() {
         randomMinMax(0, 255) + ')';
 }
 
+//定义小球类
 function Ball(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
@@ -23,6 +25,7 @@ function Ball(x, y, velX, velY, color, size) {
     this.size = size;
 }
 
+//绘制小球
 Ball.prototype.draw = function () {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -30,6 +33,7 @@ Ball.prototype.draw = function () {
     ctx.fill();
 }
 
+//使小球动起来
 Ball.prototype.update = function () {
     if ((this.x + this.size) >= width) {
         this.velX = -(this.velX);
@@ -46,8 +50,24 @@ Ball.prototype.update = function () {
     this.x += this.velX;
     this.y += this.velY;
 }
+
+//碰撞后修改对应小球的颜色
+Ball.prototype.collisionDetect = function () {
+    for (let i = 0; i < balls.length; i++) {
+        if (this !== balls[i]) {
+            const dx = this.x - balls[i].x;
+            const dy = this.y - balls[i].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < this.size + balls[i].size) {
+                balls[i].color = this.color = randomColor();
+            }
+        }
+    }
+}
+
+//生成所有小球
 let balls = [];
-while (balls.length < 25) {
+while (balls.length < 50) {
     let size = randomMinMax(10, 20);
     let ball = new Ball(
         randomMinMax(0 + size, width - size),
@@ -59,14 +79,17 @@ while (balls.length < 25) {
     )
     balls.push(ball);
 }
+
+//绘制并执行动画
 function loop() {
     ctx.fillStyle = 'rgba(0,0,0,0.25)';
     ctx.fillRect(0, 0, width, height);
     for (let i = 0; i < balls.length; i++) {
-
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
     }
+    requestAnimationFrame(loop);
 }
 
-function loop01() {
-    
-}
+loop()
